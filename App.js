ee9758f98117761d06381009ee1e44d83ca3d8e0
Abdur Rakib Rony <65207907+ronyfr3@users.noreply.github.com>
2020-10-17 02:54:18 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="section">
+            <div className="wave wave1"></div>
+            <div className="wave wave2"></div>
+            <div className="wave wave3"></div>
+            <div className="wave wave4"></div>
+        </div>
+    )
+}
+
+export default App
